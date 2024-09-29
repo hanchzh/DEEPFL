49ddf4172558a0a5ecaebc0088f3b276e47254e9
hanchzh <36350242+hanchzh@users.noreply.github.com>
2024-09-30 03:41:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# DEEPFL
-Code for the paper "Securing Marine Edge Computing: A Differential Evolution Framework for Privacy Protection and Poisoning Attack Defense"
+## Requirements
+python>=3.6  
+pytorch>=0.4
+
+## Run
+
+
+
+Federated learning with MLP and CNN is produced by:
+> python [main_fed.py](back.py)
+
+See the arguments in [options.py](utils/options.py). 
+
+For example:
+> python main_fed.py --dataset mnist --iid --num_channels 1 --model cnn --epochs 50 --gpu 0  
+
+`--all_clients` for averaging over all client models
+
+NB: for CIFAR-10, `num_channels` must be 3.
+
---
+++ main_cfed.py
@@ -0,0 +1,54 @@
+from utils import cfed_options
+from models.Update import LocalUpdate
+from utils.options import args_parser
+import copy
+from utils.Fed import Fed
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    args = args_parser()
+    loss_train = []
+    acc_test = []
+    cv_loss, cv_acc = [], []
+    val_loss_pre, counter = 0, 0
+    net_best = None
+    best_loss = None
+    val_acc_list, net_list = [], []
+    # ________model_preparing________
+    # dict_users, 每个用户所持有的数据集，这里实际上是做了一个数据划分的list
+    fed = Fed(args)
+    dataset_trains = []
+    dataset_tests = []
+    for i in range(args.num_users):
+        dataset_train, dataset_test, dict_users = fed.load_data_fl(i*0.55)
+        dataset_trains.append(dataset_train)
+        dataset_tests.append(dataset_test)
+
+    net_glob = fed.build_model()
+
+    w_glob = net_glob.state_dict()
+    Locals = []
+    for idx in range(args.num_users):
+        net_local = fed.build_model()
+        # 每个迭代轮次本地更新
+        local = LocalUpdate(
+            args=args, dataset=dataset_trains[idx], idxs=dict_users[idx], net=net_local)
+        Locals.append(local)
+    fed.Locals = Locals
+    fed.dataset_trains = dataset_trains
+    fed.dataset_tests = dataset_tests
+    fed_int = copy.deepcopy(fed)
+    w_locals = [w_glob for i in range(args.num_users)]
+    fed.train(copy.deepcopy(w_locals),0,4)
+    fed = copy.deepcopy(fed_int)
+    fed.train(copy.deepcopy(w_locals),0,3)
+    fed = copy.deepcopy(fed_int)
+    fed.train(copy.deepcopy(w_locals),0,2)
+    fed = copy.deepcopy(fed_int)
+    fed.train(copy.deepcopy(w_locals), 0, 1)
+    fed = copy.deepcopy(fed_int)
+    fed.train(copy.deepcopy(w_locals),0,0)
+
+
+
